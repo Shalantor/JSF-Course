4e8fa4bc39627620fbe8b95ba5756c4994943e6b
shalantor <gkaraolanis@uth.gr>
2016-09-14 18:29:13 +0300

    Add second exercise code

---
+++ JSF-Basics/src/flowBean.java
@@ -0,0 +1,34 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class flowBean {
+
+	public String firstRandomPage(){
+		if( Math.random() > 0.5 ){
+			return "second";
+		}
+		else{
+			return "defeat";
+		}
+	}
+	
+	public String secondRandomPage(){
+		if( Math.random() > 0.5 ){
+			return "third";
+		}
+		else{
+			return "defeat";
+		}
+	}
+	
+	public String thirdRandomPage(){
+		if( Math.random() > 0.5 ){
+			return "victory";
+		}
+		else{
+			return "defeat";
+		}
+	}
+	
+	
+}
