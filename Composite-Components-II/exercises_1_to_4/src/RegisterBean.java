7598d38c7ee2a0556458b5734c4103b3eb60c849
shalantor <gkaraolanis@uth.gr>
2016-10-05 16:39:57 +0300

    Add pages with input forms (Exercise 2)

---
+++ Composite-Components-II/exercises_1_to_4/src/RegisterBean.java
@@ -0,0 +1,52 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class RegisterBean {
+	
+	private String name,state,city,streetNumber,postalCode;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+
+	public void setStreetNumber(String streetNumber) {
+		this.streetNumber = streetNumber;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	public String showResults(){
+		return "results";
+	}
+
+}
