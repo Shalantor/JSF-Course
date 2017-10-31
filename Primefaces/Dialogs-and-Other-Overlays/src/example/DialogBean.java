a6fcd0daa3506d4cc6d52f11e7ff3fd4dbf81966
shalantor <protipas2@hotmail.com>
2017-10-31 19:09:15 +0200

    Primefaces: Dialogs and Other Overlays, solution for exercise 3

--- Primefaces/Dialogs-and-Other-Overlays/src/example/DialogBean.java
+++ Primefaces/Dialogs-and-Other-Overlays/src/example/DialogBean.java
@@ -6,6 +6,7 @@ import javax.faces.bean.ManagedBean;
 public class DialogBean {
 	
 	private Integer number = (int)(Math.random() * 1000);
+	private Integer number2 = (int)(Math.random() * 1000);
 
 	public Integer getNumber() {
 		return number;
@@ -13,6 +14,19 @@ public class DialogBean {
 
 	public void setNumber(Integer number) {
 		this.number = number;
+	}
+
+	public Integer getNumber2() {
+		return number2;
+	}
+
+	public void setNumber2(Integer number2) {
+		this.number2 = number2;
+	}
+	
+	public String randomize() {
+		number2 = (int)(Math.random() * 1000);
+		return(null);
 	}
 
 }
