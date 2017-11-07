97387f8248201506b474a8580837a27cdc8ff2dd
shalantor <protipas2@hotmail.com>
2017-11-07 13:44:40 +0200

    Primefaces: Themes, solution for exercise 2

---
+++ Primefaces/Themes/src/example/ThemeChoices.java
@@ -0,0 +1,28 @@
+package example;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ThemeChoices {
+	
+	public static final String[] POSSIBLE_THEMES =
+	{ "afterdark", "afternoon", 
+	"afterwork", "aristo", 
+	"black-tie", "blitzer",
+	 "bluesky", "casablanca",
+	"cruze", "cupertino", "dark-hive", "dot-luv",
+	"eggplant", "excite-bike", "flick", "glass-x",
+	"home", "hot-sneaks", 
+	"humanity", "le-frog",
+	"midnight", "mint-choc", "overcast", "pepper-grinder",
+	"redmond", "rocket", 
+	"sam", "smoothness",
+	"south-street", "start", 
+	"sunny", "swanky-purse",
+	"trontastic", "twitter bootstrap", "ui-darkness", 
+	"ui-lightness", "vader" };
+	
+	public String[] getThemes() {
+		return(POSSIBLE_THEMES);
+	}
+}
